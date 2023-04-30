1fe530a831daef66acf9931fa0621f60a28a5e83
MDWebDevelopment <md.web.development404@gmail.com>
2023-05-01 01:29:52 +0200

    Initial Commit

--- interface.py
+++
@@ -1,6 +0,0 @@
-
-from PyQt5.QtWidgets import QLabel, QGridLayout
-from main import Kalkulator
-
-class UI(Kalkulator):
-    d
--- main.py
+++ main.py
@@ -82,6 +82,7 @@ class Kalkulator(QWidget):
         EndWE.setMaximumWidth(100)
         EndWE.addItem("West")
         EndWE.addItem("East")
+        Distance = QLabel()
 
         ukladT.addWidget(EndLatitude, 0, 3)
         ukladT.addWidget(EndLongitude, 1, 3)
@@ -90,6 +91,9 @@ class Kalkulator(QWidget):
         ukladT.addWidget(PlanetSide1, 2, 3)
         ukladT.addWidget(EndNS, 2, 4)
         ukladT.addWidget(EndWE, 2, 5)
+        ukladT.addWidget(Distance, 3, 3)
+
+
 
 
 
@@ -105,91 +109,61 @@ class Kalkulator(QWidget):
         import plotly.graph_objects as go
 
         def Calculation():
+
             B1 = float(StartLatitudeInput.text())
             L1 = float(StartLongitudeInput.text())
-            StartHemiNS = StartNS.text()
-            StartHemiWE = StartWE.text()
+            print(B1, L1)
+            StartHemiNS = StartNS.currentData()
+            StartHemiWE = StartWE.currentData()
             B2 = float(EndLatitudeInput.text())
             L2 = float(EndLongitudeInput.text())
-            EndHemiNS = EndNS.text()
-            EndHemiWE = EndWE.text()
+            EndHemiNS = EndNS.currentData()
+            EndHemiWE = EndWE.currentData()
+            print(StartHemiNS)
 
             a = 6378137
             b = 6356752.3141
-            e2 = (a ** 2 - b ** 2) / a ** 2
+            e2 = (a**2 - b**2)/a**2
             f = (a - b) / a
-            B_p = B1 * np.pi / 180
-            L_p = L1 * np.pi / 180
-            B_k = B2 * np.pi / 180  # współrzędne z 12c
-            L_k = L2 * np.pi / 180
+            B_p = B1*np.pi/180
+            L_p = L1*np.pi/180
+            B_k = B2*np.pi/180
+            L_k = L2*np.pi/180
+
             print('B_p: ', B_p, B_p * 180 / np.pi, 'L_p: ', L_p, L_p * 180 / np.pi)
             print('B_k: ', B_k, B_k * 180 / np.pi, 'L_k: ', L_k, L_k * 180 / np.pi)
 
-            # wzór 71
             U1 = np.arctan((1 - f) * np.tan(B_p))
-            # wzór 72
             U2 = np.arctan((1 - f) * np.tan(B_k))
-            print('U1: ', U1)
-            print('U2: ', U2)
-            # wzór 73
+
             L = L_k - L_p
-            print('L: ', L)
             Lambda = L
+
             for i in range(10):
-                print('i: ', i)
                 Lambda_prev = Lambda
-                # wzór 74
-                sinSigma =np.sqrt((np.cos(U2) * np.sin(Lambda)) ** 2 + (np.cos(U1) * np.sin(U2) - np.sin(U1) * np.cos(U2) * np.cos(Lambda)) ** 2)
-
-
-                print('sinSigma: ', sinSigma)
-                # wzór 75
+                sinSigma = np.sqrt((np.cos(U2)*np.sin(Lambda))**2+(np.cos(U1)*np.sin(U2)-np.sin(U1)*np.cos(U2)*np.cos(Lambda))**2)
                 cosSigma = np.sin(U1) * np.sin(U2) + np.cos(U1) * np.cos(U2) * np.cos(Lambda)
-                print('cosSigma: ', cosSigma)
-                # wzór 76
                 sigma = np.arctan2(sinSigma, cosSigma)
-                print('sigma: ', sigma)
-                # wzór 77
                 sinAlfa = np.cos(U1) * np.cos(U2) * np.sin(Lambda) / sinSigma
-                print('sinAlfa: ', sinAlfa)
-                # wzór 78
                 cos2Sigmam = cosSigma - (2 * np.sin(U1) * np.sin(U2) / (1 - sinAlfa ** 2))
-                print('cos2Sigmam: ', cos2Sigmam)
-                # wzór 79
                 C = f / 16 * (1 - sinAlfa ** 2) * (4 + f * (4 - 3 * (1 - sinAlfa ** 2)))
-                print('C: ', C)
-
-                # wzór 80
-                Lambda = L + (1 - C) * f * sinAlfa *(sigma + C * sinSigma * (cos2Sigmam + C * cosSigma * (-1 + 2 * cos2Sigmam ** 2)))
-
-                print('Lambda: ', Lambda)
-                print('LambdaPrev: ', Lambda_prev)
+                Lambda = L + (1 - C) * f * sinAlfa *(sigma+C*sinSigma*(cos2Sigmam+C*cosSigma*(-1+2*cos2Sigmam**2)))
                 if abs(Lambda - Lambda_prev) < 1e-12:
-                    break  # koniec pętli
-                # wzór 81
+                    break
                 u2 = (1 - sinAlfa ** 2) * (a ** 2 - b ** 2) / (b ** 2)
-                print('u2: ', u2)
-                # wzór 82
                 A = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
-                print('A: ', A)
-                # wzór 83
                 B = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
-                print('B: ', B)
-                # wzór 84
-                deltaSigma = B * sinSigma * (cos2Sigmam + B / 4 * (cosSigma *(-1 + 2 * cos2Sigmam ** 2)- B / 6 * cos2Sigmam *(-3 + 4 * sinSigma ** 2) *(-3 + 4 * cos2Sigmam ** 2)))
-
-                print("deltaSigma: ", deltaSigma)
-                # wzór 85
+                deltaSigma = B * sinSigma * (cos2Sigmam + B / 4 * (cosSigma *(-1+2*cos2Sigmam**2)-B/6*cos2Sigmam*(-3+4*sinSigma**2)*(-3+4*cos2Sigmam**2)))
                 s = b * A * (sigma - deltaSigma)
-                print('s: ', s)
-                # wzór 86
-                Az_12 = np.arctan2((np.cos(U2) * np.sin(Lambda)),
-                                   (np.cos(U1) * np.sin(U2) - np.sin(U1) * np.cos(U2) * np.cos(Lambda)))
+                Az_12 = np.arctan2((np.cos(U2) * np.sin(Lambda)),(np.cos(U1)*np.sin(U2)-np.sin(U1)*np.cos(U2)*np.cos(Lambda)))
+                Az_21 = np.arctan2((np.cos(U1) * np.sin(Lambda)),((-np.sin(U1)) * np.cos(U2) + np.cos(U1) * np.sin(U2) * np.cos(Lambda)))
                 print('Az_12', (Az_12) * 180 / np.pi)
-                # wzór 87
-                Az_21 = np.arctan2((np.cos(U1) * np.sin(Lambda)),
-                                   ((-np.sin(U1)) * np.cos(U2) + np.cos(U1) * np.sin(U2) * np.cos(Lambda)))
                 print('Az_21', (np.pi + Az_21) * 180 / np.pi)
+
+                Distance.setText(f'Distance: {s} meters')
+
+
+
 
         calculateButton.clicked.connect(Calculation)
 
@@ -199,7 +173,7 @@ class Kalkulator(QWidget):
         height = 800
         self.setGeometry(int(m.width/2-width/2), int(m.height/2-height/2), width, height)
         self.setWindowIcon(QIcon('kalkulator.png'))
-        self.setWindowTitle("Prosty kalkulator")
+        self.setWindowTitle("Vincent's Inverse Formula")
         self.show()
 
 
