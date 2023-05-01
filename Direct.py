2c823efaa7b8965a338cfccb26ce640a7761b681
MDWebDevelopment <md.web.development404@gmail.com>
2023-05-01 14:36:14 +0200

    Finalizing

---
+++ Direct.py
@@ -0,0 +1,160 @@
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+    QGridLayout,
+)
+from PyQt5.QtGui import QIcon
+from screeninfo import get_monitors
+
+class Direct(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+        global m
+        for m in get_monitors():
+            continue
+
+        grid = QGridLayout()
+
+
+        Latitude = QLabel("Latitude: ")
+        Latitude.setMaximumWidth(100)
+        LatitudeInput = QLineEdit()
+        LatitudeInput.setMaximumWidth(100)
+        grid.addWidget(Latitude, 0, 0)
+        grid.addWidget(LatitudeInput, 0, 1)
+
+        Longitude = QLabel("Longitude: ")
+        Longitude.setMaximumWidth(100)
+        LongitudeInput = QLineEdit()
+        LongitudeInput.setMaximumWidth(100)
+        grid.addWidget(LongitudeInput, 1, 1)
+        grid.addWidget(Longitude, 1, 0)
+
+        HemiNS = QComboBox()
+        HemiNS.setMaximumWidth(100)
+        HemiNS.addItem('North')
+        HemiNS.addItem('South')
+        grid.addWidget(HemiNS, 0, 2)
+
+        HemiWE = QComboBox()
+        HemiWE.setMaximumWidth(100)
+        HemiWE.addItem('West')
+        HemiWE.addItem('East')
+        grid.addWidget(HemiWE, 1, 2)
+
+        Azimuth1_2 = QLabel('Azimuth from point 1 to 2: ')
+        Azimuth1_2Input = QLineEdit()
+        Azimuth1_2.setMaximumWidth(100)
+        Azimuth1_2Input.setMaximumWidth(100)
+        grid.addWidget(Azimuth1_2, 2,0)
+        grid.addWidget(Azimuth1_2Input, 2, 1)
+
+        ElipsoidalDistance = QLabel("Ellispoidal Distance(in metres): ")
+        ElipsoidalDistanceInput = QLineEdit()
+        ElipsoidalDistance.setMaximumWidth(200)
+        ElipsoidalDistanceInput.setMaximumWidth(100)
+        grid.addWidget(ElipsoidalDistance, 2, 2)
+        grid.addWidget(ElipsoidalDistanceInput, 2, 3)
+
+        Calculate = QPushButton('Calculate')
+        grid.addWidget(Calculate, 3, 1)
+
+        plot = QtWebEngineWidgets.QWebEngineView()
+
+        import numpy as np
+        def Calculation():
+
+            B = float(LatitudeInput.text())
+            L = float(LongitudeInput.text())
+            Az1_2 = float(Azimuth1_2Input.text())
+            NS = HemiNS.currentText()
+            WE = HemiWE.currentText()
+            print(NS, WE)
+            if NS == 'North':
+                B = B
+            if NS == 'South':
+                B = 0-B
+            if WE == 'West':
+                L = 0-L
+            if WE == 'East':
+                L = L
+            print(B, L)
+
+            a_GRS80 = 6378137
+            b_GRS80 = 6356752.3141
+            e2_GRS80 = (a_GRS80 ** 2 - b_GRS80 ** 2) / a_GRS80 ** 2
+            B = B * np.pi / 180
+            L = B * np.pi / 180
+            Az = Az1_2*np.pi/180
+
+            s = float(ElipsoidalDistanceInput.text())
+            N = a_GRS80 / np.sqrt(1 - e2_GRS80 * np.sin(B) ** 2)
+            M = (1 - e2_GRS80) * a_GRS80 / (np.sqrt(1 - e2_GRS80 * np.sin(B) ** 2)) ** 3
+
+            ds = 1000
+            s1 = 0
+            PB = [B * 180/np.pi]
+            PL = [L * 180/np.pi]
+
+            while s1<s:
+                Np = N
+                Mp = M
+                Bp = B
+                dL = ds * np.sin(Az) / (Np * np.cos(Bp))
+                L = L + dL
+                dB = ds * np.cos(Az) / Mp
+                B = B + dB
+                N = a_GRS80 / np.sqrt(1 - e2_GRS80 * np.sin(B) ** 2)
+                M = (1 - e2_GRS80) * a_GRS80 / (np.sqrt(1 - e2_GRS80 * np.sin(B) ** 2)) ** 3
+                Az = Az + ds / Np * np.sin(Az) * np.tan(Bp)
+                s1 = s1 + ds
+                print(B * 180 / np.pi, L * 180 / np.pi, Az * 180 / np.pi, s1)
+                print(B * 180 / np.pi, L * 180 / np.pi, Az * 180 / np.pi, s1)
+                PB.extend([B * 180 / np.pi])
+                PL.extend([L * 180 / np.pi])
+
+            print('B_b: ', B * 180 / np.pi, 'L_b: ', L * 180 / np.pi, 'Az_b: ',
+                  Az * 180 / np.pi,
+                  'Azymut odwrotny: ', (Az + np.pi) * 180 / np.pi)
+            B_b = B
+            L_b = L
+            Az_b = Az
+
+            import plotly.graph_objects as go
+            fig = go.Figure(go.Scattermapbox(mode="lines", lat=PB, lon=PL, marker={'size': 10}))
+            fig.data[0].line.color = 'rgb(204, 20, 204)'
+            fig.update_layout(margin={'l': 0, 't': 0, 'b': 0, 'r': 0},mapbox={"center": {'lon': 150, 'lat': 150}, "style": "stamen-terrain","center": {'lon': 50, 'lat': 50}, "zoom": 1})
+            fig.show()
+
+
+
+        Calculate.clicked.connect(Calculation)
+        self.setLayout(grid)
+        width = 1000
+        height = 800
+        self.setGeometry(int(m.width / 2 - width / 2), int(m.height / 2 - height / 2), width, height)
+        self.setWindowIcon(QIcon('kalkulator.png'))
+        self.setWindowTitle("Vincent's Inverse Formula")
+        self.show()
---
+++ Inverse.py
@@ -0,0 +1,179 @@
+from PyQt5.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+    QGridLayout,
+)
+from PyQt5.QtGui import QIcon
+from screeninfo import get_monitors
+
+
+
+class Inverse(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.interfejs()
+
+    def interfejs(self):
+        for m in get_monitors():
+            continue
+        ukladT = QGridLayout()
+        #Starting Coordinates
+        StartLatitude = QLabel("Starting Latitude:", self)
+        StartLatitude.setMaximumWidth(100)
+        StartLongitude = QLabel("Starting Longitude:", self)
+        StartLongitude.setMaximumWidth(100)
+        StartLatitudeInput = QLineEdit(self)
+        StartLatitudeInput.setMaximumWidth(100)
+        StartLongitudeInput = QLineEdit(self)
+        StartLongitudeInput.setMaximumWidth(100)
+        StartNS = QComboBox(self)
+        StartNS.setMaximumWidth(100)
+        PlanetSide1 = QLabel("Starting point hemispheres: ")
+        StartNS.addItem("North")
+        StartNS.addItem("South")
+        StartWE = QComboBox(self)
+        StartWE.setMaximumWidth(100)
+        StartWE.addItem("West")
+        StartWE.addItem("East")
+
+        ukladT.addWidget(StartLatitude, 0, 0)
+        ukladT.addWidget(StartLongitude, 1, 0)
+        ukladT.addWidget(StartLatitudeInput, 0, 1)
+        ukladT.addWidget(StartLongitudeInput, 1, 1)
+        ukladT.addWidget(PlanetSide1, 2, 0)
+        ukladT.addWidget(StartNS, 2, 1)
+        ukladT.addWidget(StartWE, 2, 2)
+
+        #Ending Coordinates
+        EndLatitude = QLabel("Ending Latitude:", self)
+        EndLatitude.setMaximumWidth(100)
+        EndLongitude = QLabel("Ending Longitude:", self)
+        EndLongitude.setMaximumWidth(100)
+        EndLatitudeInput = QLineEdit(self)
+        EndLatitudeInput.setMaximumWidth(100)
+        EndLongitudeInput = QLineEdit(self)
+        EndLongitudeInput.setMaximumWidth(100)
+        EndNS = QComboBox(self)
+        EndNS.setMaximumWidth(100)
+        PlanetSide1 = QLabel("Ending point hemispheres: ")
+        EndNS.addItem("North")
+        EndNS.addItem("South")
+        EndWE = QComboBox(self)
+        EndWE.setMaximumWidth(100)
+        EndWE.addItem("West")
+        EndWE.addItem("East")
+        Distance = QLabel()
+
+        ukladT.addWidget(EndLatitude, 0, 3)
+        ukladT.addWidget(EndLongitude, 1, 3)
+        ukladT.addWidget(EndLatitudeInput, 0, 4)
+        ukladT.addWidget(EndLongitudeInput, 1, 4)
+        ukladT.addWidget(PlanetSide1, 2, 3)
+        ukladT.addWidget(EndNS, 2, 4)
+        ukladT.addWidget(EndWE, 2, 5)
+        ukladT.addWidget(Distance, 3, 3)
+
+
+
+
+
+
+        calculateButton = QPushButton("Calculate", self)
+        ukladT.addWidget(calculateButton, 3, 2)
+
+
+
+
+        #Calculations
+        import numpy as np
+        import plotly.graph_objects as go
+
+        def Calculation():
+
+            B1 = float(StartLatitudeInput.text())
+            L1 = float(StartLongitudeInput.text())
+            print(B1, L1)
+            StartHemiNS = StartNS.currentData()
+            StartHemiWE = StartWE.currentData()
+            B2 = float(EndLatitudeInput.text())
+            L2 = float(EndLongitudeInput.text())
+            EndHemiNS = EndNS.currentData()
+            EndHemiWE = EndWE.currentData()
+            print(StartHemiNS)
+
+            a = 6378137
+            b = 6356752.3141
+            e2 = (a**2 - b**2)/a**2
+            f = (a - b) / a
+            B_p = B1*np.pi/180
+            L_p = L1*np.pi/180
+            B_k = B2*np.pi/180
+            L_k = L2*np.pi/180
+
+            print('B_p: ', B_p, B_p * 180 / np.pi, 'L_p: ', L_p, L_p * 180 / np.pi)
+            print('B_k: ', B_k, B_k * 180 / np.pi, 'L_k: ', L_k, L_k * 180 / np.pi)
+
+            U1 = np.arctan((1 - f) * np.tan(B_p))
+            U2 = np.arctan((1 - f) * np.tan(B_k))
+
+            L = L_k - L_p
+            Lambda = L
+
+            for i in range(10):
+                Lambda_prev = Lambda
+                sinSigma = np.sqrt((np.cos(U2)*np.sin(Lambda))**2+(np.cos(U1)*np.sin(U2)-np.sin(U1)*np.cos(U2)*np.cos(Lambda))**2)
+                cosSigma = np.sin(U1) * np.sin(U2) + np.cos(U1) * np.cos(U2) * np.cos(Lambda)
+                sigma = np.arctan2(sinSigma, cosSigma)
+                sinAlfa = np.cos(U1) * np.cos(U2) * np.sin(Lambda) / sinSigma
+                cos2Sigmam = cosSigma - (2 * np.sin(U1) * np.sin(U2) / (1 - sinAlfa ** 2))
+                C = f / 16 * (1 - sinAlfa ** 2) * (4 + f * (4 - 3 * (1 - sinAlfa ** 2)))
+                Lambda = L + (1 - C) * f * sinAlfa *(sigma+C*sinSigma*(cos2Sigmam+C*cosSigma*(-1+2*cos2Sigmam**2)))
+                if abs(Lambda - Lambda_prev) < 1e-12:
+                    break
+                u2 = (1 - sinAlfa ** 2) * (a ** 2 - b ** 2) / (b ** 2)
+                A = 1 + u2 / 16384 * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)))
+                B = u2 / 1024 * (256 + u2 * (-128 + u2 * (74 - 47 * u2)))
+                deltaSigma = B * sinSigma * (cos2Sigmam + B / 4 * (cosSigma *(-1+2*cos2Sigmam**2)-B/6*cos2Sigmam*(-3+4*sinSigma**2)*(-3+4*cos2Sigmam**2)))
+                s = b * A * (sigma - deltaSigma)
+                Az_12 = np.arctan2((np.cos(U2) * np.sin(Lambda)),(np.cos(U1)*np.sin(U2)-np.sin(U1)*np.cos(U2)*np.cos(Lambda)))
+                Az_21 = np.arctan2((np.cos(U1) * np.sin(Lambda)),((-np.sin(U1)) * np.cos(U2) + np.cos(U1) * np.sin(U2) * np.cos(Lambda)))
+                print('Az_12', (Az_12) * 180 / np.pi)
+                print('Az_21', (np.pi + Az_21) * 180 / np.pi)
+
+                Distance.setText(f'Distance: {s} meters')
+
+
+
+
+        calculateButton.clicked.connect(Calculation)
+
+        # przypisanie utworzonego układu do okna
+        self.setLayout(ukladT)
+        width = 1000
+        height = 800
+        self.setGeometry(int(m.width/2-width/2), int(m.height/2-height/2), width, height)
+        self.setWindowIcon(QIcon('kalkulator.png'))
+        self.setWindowTitle("Vincent's Inverse Formula")
+        self.show()
+
+
+
